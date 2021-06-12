a16436cf3db4f5b4a1fe625e445e8eb9f782a8b2
Boshra <boshrah0912@gmail.com>
2021-06-12 21:43:43 +0300

    dsd

--- lib/Home/homepage.dart
+++ lib/Home/homepage.dart
@@ -1,6 +1,5 @@
-import 'package:b/Home/show.dart';
-import 'package:b/Home/test.dart';
 
+import 'package:b/Home/Company_Pages/Company_Page.dart';
 import 'package:b/myDrawer/Drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
@@ -21,9 +20,10 @@ class _MyHomePageState extends State<MyHomePage> {
   var docid, userInfo;
 
   List All_jobs = [];
-  List My_jobs=[];
+  List My_jobs = [];
 
-  CollectionReference jobsref = FirebaseFirestore.instance.collection("company");
+  CollectionReference jobsref = FirebaseFirestore.instance.collection(
+      "company");
 
   /////////////////////////////get all data
   get_All_data() async {
@@ -32,24 +32,29 @@ class _MyHomePageState extends State<MyHomePage> {
       if (this.mounted) {
         setState(() {
           All_jobs.add(element.data());
-        });}});
+        });
+      }
+    });
     print(All_jobs.length);
   }
 
 
   /////////////////////////////get my chance
-  get_My_data()async {
-      await jobsref.where("degree",isEqualTo: "اعدادي").get().then((value) => {
-        value.docs.forEach((element) {
-          if (this.mounted) {
-            setState(() {
-              My_jobs.add(element.data());
-            });}})});
-      print(My_jobs.length);
-    }
+  get_My_data() async {
+    await jobsref.where("degree", isEqualTo: "اعدادي").get().then((value) =>
+    {
+      value.docs.forEach((element) {
+        if (this.mounted) {
+          setState(() {
+            My_jobs.add(element.data());
+          });
+        }
+      })});
+    print(My_jobs.length);
+  }
 
 
-  getId(){
+  getId() {
     FirebaseFirestore.instance
         .collection('users')
         .get()
@@ -57,10 +62,14 @@ class _MyHomePageState extends State<MyHomePage> {
       querySnapshot.docs.forEach((doc) {
         if (doc['uid'] == FirebaseAuth.instance.currentUser.uid) {
           if (this.mounted) {
-          setState(() {
-            userInfo = doc.data();
-            docid = doc.id;
-          });}}});});
+            setState(() {
+              userInfo = doc.data();
+              docid = doc.id;
+            });
+          }
+        }
+      });
+    });
   }
 
 
@@ -68,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     get_All_data();
     get_My_data();
-        getId();
+    getId();
     super.initState();
   }
 
@@ -77,82 +86,88 @@ class _MyHomePageState extends State<MyHomePage> {
     return Directionality(
         textDirection: TextDirection.rtl,
         child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: DefaultTabController(length:3,
-        child:
-        Directionality(
-        textDirection: TextDirection.rtl,
-            child: Scaffold(
-              appBar: AppBar( backgroundColor:  Colors.pink.shade800,
+            debugShowCheckedModeBanner: false,
+            home: DefaultTabController(length: 3,
+                child:
+                Directionality(
+                    textDirection: TextDirection.rtl,
+                    child: Scaffold(
+                      appBar: AppBar(backgroundColor: Colors.pink.shade900,
+                        toolbarHeight: 150,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                              //  topRight: Radius.circular(10),
+                              //  topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(100),
+                              bottomRight: Radius.circular(70),)),
 
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(
-                      //  topRight: Radius.circular(10),
-                      //  topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(100),
-                        bottomRight: Radius.circular(70),)),
+                        bottom: TabBar(indicatorColor: Colors.white,
+                          indicator: UnderlineTabIndicator(
+                            //  borderSide: BorderSide(width: 10.0),
+                              insets: EdgeInsets.symmetric(horizontal: 60.0,)
+                          ),
 
-                bottom: TabBar(indicatorColor:Colors.white ,
-                  indicator: UnderlineTabIndicator(
-                    //  borderSide: BorderSide(width: 10.0),
-                      insets: EdgeInsets.symmetric(horizontal:60.0,)
-                  ),
+                          tabs: [
+                            Tab(child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text("جميع الفرص"),),),
+                            Tab(child: Text("فرصي"),),
 
-                  tabs: [
-                    Tab(child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("جميع الفرص"),),),
-                    Tab(child: Text("فرصي"),),
+                            Tab(child: Align(
+                              alignment: Alignment.centerRight,
+                              child: Text("الشركات"),),)
+                          ],
+                        ),
+                        title: TextField(
+                         // autofocus: true,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.fromLTRB(0.1, 0.1, 20, 0.1),
+                              filled: true,
+                              fillColor: Colors.white,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.white, width: 3.0,),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.pink.shade800, width: 1.0,),
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(30.0)),
+                              ),
 
-                    Tab(child: Align(
-                      alignment: Alignment.centerRight,child: Text("الشركات"),),)],
-                ),
-                title: TextField(
-                  autofocus: true,
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white, width: 3.0,),
+                              hintText: "البحث ",
+                              border: InputBorder.none,
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.search), onPressed: () {},)
+                          ),
+                          style: TextStyle(color: Colors.white, fontSize: 14.0),
+                        ),
+                        iconTheme: IconThemeData(color: Colors.white),
+
+                        //backgroundColor: Colors.white,
+                        actions: <Widget>[
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications_active,
+                              color: Colors.amberAccent,
+                            ),
+                            onPressed: () {
+                              //do some things to show notifications
+                            },
+                          ),
+                        ],
                       ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink.shade800, width: 3.0,),
-                        borderRadius: BorderRadius.all(
-                            Radius.circular(30.0)),
+
+                      drawer: mydrawer(userInfo: userInfo, docid: docid,),
+
+                      body: TabBarView(
+                        children: [
+                          all_chance(All_jobs),
+                          my_chance(My_jobs),
+                          companyPage(),
+                        ],
                       ),
-
-                      hintText: "البحث ",
-                      border: InputBorder.none,
-                      suffixIcon: IconButton(icon:Icon(Icons.search), onPressed: () {
-                      },)
-                  ),
-                  style: TextStyle(color: Colors.white, fontSize: 14.0),
-                ),
-                iconTheme: IconThemeData(color: Colors.white),
-
-                //backgroundColor: Colors.white,
-                actions: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.notifications_active,
-                      color: Colors.amberAccent,
-                    ),
-                    onPressed: (){
-                      //do some things to show notifications
-                    },
-                  ),
-                ],
-              ),
-
-              drawer: mydrawer(userInfo: userInfo, docid: docid,),
-
-              body:TabBarView(
-                children: [
-                  all_chance(All_jobs),
-                  my_chance(My_jobs),
-                  my_chance(My_jobs),
-                ],
-              ),
-            )
-        ))));
+                    )
+                ))));
   }
 }
