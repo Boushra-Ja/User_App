94a1396aedfcb31eaec8310d7da5aac8360c0c66
Boshra <boshrah0912@gmail.com>
2021-07-18 16:44:14 +0300

    Edit home and appbar

--- lib/Home/appbar.dart
+++ lib/Home/appbar.dart
@@ -7,70 +7,109 @@ class myappbar extends StatelessWidget  implements PreferredSize{
 
   @override
   Widget build(BuildContext context) {
-    return  AppBar(backgroundColor: Colors.pink.shade900,
-        toolbarHeight: 150,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(
-          //  topRight: Radius.circular(10),
-          //  topLeft: Radius.circular(10),
-          bottomLeft: Radius.circular(100),
-          bottomRight: Radius.circular(70),)),
+    return  AppBar(
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+                  Colors.pink.shade900,
+                  Colors.grey.shade800
+                ])),
+      ),
+      toolbarHeight: 150,
+      /* shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                          //  topRight: Radius.circular(10),
+                          //  topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(100),
+                          bottomRight: Radius.circular(70),
+                        )),*/
 
-        bottom: TabBar(indicatorColor: Colors.white,
-          indicator: UnderlineTabIndicator(
-            //  borderSide: BorderSide(width: 10.0),
-              insets: EdgeInsets.symmetric(horizontal: 60.0,)
-          ),
-
-          tabs: [
-            Tab(child: Align(
+      bottom: TabBar(
+        indicatorColor: Colors.white,
+        indicator: UnderlineTabIndicator(
+          //  borderSide: BorderSide(width: 10.0),
+            insets: EdgeInsets.symmetric(
+              horizontal: 60.0,
+            )),
+        tabs: [
+          Tab(
+            child: Align(
               alignment: Alignment.centerLeft,
-              child: Text("جميع الفرص"),),),
-            Tab(child: Text("فرصي"),),
-
-            Tab(child: Align(
-              alignment: Alignment.centerRight, child: Text("الشركات"),),)
-          ],
-        ),
-        title: TextField(
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(0.1, 0.1, 20, 0.1),
-              filled: true,
-              fillColor: Colors.white,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.white, width: 3.0,),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.pink.shade800, width: 3.0,),
-                borderRadius: BorderRadius.all(
-                    Radius.circular(30.0)),
-              ),
-
-              hintText: "البحث ",
-              border: InputBorder.none,
-              suffixIcon: IconButton(
-                icon: Icon(Icons.search), onPressed: () {},)
-          ),
-          style: TextStyle(color: Colors.white, fontSize: 14.0),
-        ),
-        iconTheme: IconThemeData(color: Colors.white),
-
-        //backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.notifications_active,
-              color: Colors.amberAccent,
+              child: Icon(Icons.work),
             ),
-            onPressed: () {
-              //do some things to show notifications
-            },
           ),
+          Tab(
+            child: Icon(Icons.favorite),
+          ),
+          Tab(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.business),
+            ),
+          ),
+          Tab(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.markunread_sharp),
+            ),
+          ),
+          Tab(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.lightbulb),
+            ),
+          )
         ],
-      );
+      ),
+      title: TextField(
+        decoration: InputDecoration(
+            contentPadding:
+            EdgeInsets.fromLTRB(0.1, 0.1, 20, 0.1),
+            filled: true,
+            fillColor: Colors.white,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.pink.shade800,
+                width: 1.0,
+              ),
+              borderRadius:
+              BorderRadius.all(Radius.circular(30.0)),
+            ),
+            hintText: "البحث ",
+            border: InputBorder.none,
+            suffixIcon: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )),
+        style: TextStyle(color: Colors.white, fontSize: 14.0),
+      ),
+      iconTheme: IconThemeData(color: Colors.white),
+
+      //backgroundColor: Colors.white,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.notifications_active,
+            color: Colors.amberAccent,
+          ),
+          onPressed: () {
+            //do some things to show notifications
+          },
+        ),
+      ],
+    );
 
   }
   @override
-  Size get preferredSize => new Size.fromHeight(kToolbarHeight+40);
+  Size get preferredSize => new Size.fromHeight(kToolbarHeight + 100);
   Widget get child => throw UnimplementedError();
 }
--- lib/Home/homepage.dart
+++ lib/Home/homepage.dart
@@ -10,6 +10,7 @@ import 'Company_Pages/Company_Page.dart';
 import 'Leatest_New/Refrech_Posts.dart';
 import 'my_chance.dart';
 import 'all_chance.dart';
+import 'package:b/Home/appbar.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -22,7 +23,7 @@ class _MyHomePageState extends State<MyHomePage> {
   List All_jobs = [];
   List My_jobs = [];
   bool loading = true;
-  CollectionReference jobsref = FirebaseFirestore.instance.collection("jobs");
+  CollectionReference jobsref = FirebaseFirestore.instance.collection("companies");
 
   /////////////////////////////get all data
   get_All_data() async {
@@ -97,7 +98,6 @@ class _MyHomePageState extends State<MyHomePage> {
     print(user.firstName);
   }
 
-
   @override
   void initState() {
     setState(() {
@@ -127,106 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Directionality(
                     textDirection: TextDirection.rtl,
                     child: Scaffold(
-                      appBar: AppBar(
-                        flexibleSpace: Container(
-                          decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                  begin: Alignment.topLeft,
-                                  end: Alignment.bottomRight,
-                                  colors: <Color>[
-                                Colors.pink.shade900,
-                                Colors.grey.shade800
-                              ])),
-                        ),
-                        toolbarHeight: 150,
-                        /* shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                          //  topRight: Radius.circular(10),
-                          //  topLeft: Radius.circular(10),
-                          bottomLeft: Radius.circular(100),
-                          bottomRight: Radius.circular(70),
-                        )),*/
-
-                        bottom: TabBar(
-                          indicatorColor: Colors.white,
-                          indicator: UnderlineTabIndicator(
-                              //  borderSide: BorderSide(width: 10.0),
-                              insets: EdgeInsets.symmetric(
-                            horizontal: 60.0,
-                          )),
-                          tabs: [
-                            Tab(
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Icon(Icons.work),
-                              ),
-                            ),
-                            Tab(
-                              child: Icon(Icons.favorite),
-                            ),
-                            Tab(
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Icon(Icons.business),
-                              ),
-                            ),
-                            Tab(
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Icon(Icons.markunread_sharp),
-                              ),
-                            ),
-                            Tab(
-                              child: Align(
-                                alignment: Alignment.centerRight,
-                                child: Icon(Icons.lightbulb),
-                              ),
-                            )
-                          ],
-                        ),
-                        title: TextField(
-                          decoration: InputDecoration(
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(0.1, 0.1, 20, 0.1),
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 3.0,
-                                ),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.pink.shade800,
-                                  width: 1.0,
-                                ),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(30.0)),
-                              ),
-                              hintText: "البحث ",
-                              border: InputBorder.none,
-                              suffixIcon: IconButton(
-                                icon: Icon(Icons.search),
-                                onPressed: () {},
-                              )),
-                          style: TextStyle(color: Colors.white, fontSize: 14.0),
-                        ),
-                        iconTheme: IconThemeData(color: Colors.white),
-
-                        //backgroundColor: Colors.white,
-                        actions: <Widget>[
-                          IconButton(
-                            icon: Icon(
-                              Icons.notifications_active,
-                              color: Colors.amberAccent,
-                            ),
-                            onPressed: () {
-                              //do some things to show notifications
-                            },
-                          ),
-                        ],
-                      ),
+                      appBar: myappbar(),
                       drawer: mydrawer(
                         user: user,
                         docid: docid,
